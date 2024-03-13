9d11fe76ffdbaefecc7a1656620929a8c6246e47
ma0048 <ma0048@technikum-wien.at>
2024-03-13 13:42:53 +0100

    - added rights.sql

---
+++ sql/1/002_rights.sql
@@ -0,0 +1,13 @@
+INSERT INTO system.tbl_berechtigung(berechtigung_kurzbz, beschreibung)
+SELECT 'extension/anwesenheit_assistenz', ''
+    WHERE
+	NOT EXISTS(SELECT 1 FROM system.tbl_berechtigung WHERE berechtigung_kurzbz='extension/anwesenheit_assistenz');
+INSERT INTO system.tbl_berechtigung(berechtigung_kurzbz, beschreibung)
+SELECT 'extension/anwesenheit_lektor', ''
+    WHERE
+    NOT EXISTS(SELECT 1 FROM system.tbl_berechtigung WHERE berechtigung_kurzbz='extension/anwesenheit_lektor');
+INSERT INTO system.tbl_berechtigung(berechtigung_kurzbz, beschreibung)
+SELECT 'extension/anwesenheit_student', ''
+    WHERE
+    NOT EXISTS(SELECT 1 FROM system.tbl_berechtigung WHERE berechtigung_kurzbz='extension/anwesenheit_student');
+
