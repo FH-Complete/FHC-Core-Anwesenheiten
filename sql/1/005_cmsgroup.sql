70fd7d79b27137e13594f3436e8c5bdee3ce2e06
Andreas Österreicher <oesi@technikum-wien.at>
2025-02-11 13:12:17 +0100

    CMS Gruppen entfernt aus Installationsscript

--- sql/1/005_cmsgroup.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO public.tbl_gruppe(gruppe_kurzbz, studiengang_kz, bezeichnung, beschreibung, sichtbar, lehre, aktiv, content_visible)
-SELECT 'ANW_EXT_STUDENT', 0, 'CMS Sichtbarkeitsgruppe Student', 'CMS Sichtbarkeitsgruppe Student', true, false, true, true
-WHERE
-	NOT EXISTS(SELECT 1 FROM public.tbl_gruppe WHERE gruppe_kurzbz='ANW_EXT_STUDENT');
-
-INSERT INTO public.tbl_gruppe(gruppe_kurzbz, studiengang_kz, bezeichnung, beschreibung, sichtbar, lehre, aktiv, content_visible)
-SELECT 'ANW_EXT_ENT_ADMIN', 0, 'CMSEntAdmin', 'CMSEntAdmin', true, false, true, true
-WHERE
-	NOT EXISTS(SELECT 1 FROM public.tbl_gruppe WHERE gruppe_kurzbz='ANW_EXT_ENT_ADMIN');
