a18162e0b71eff364dd35948b00d8b67bc3583fe
Johann Hoffmann <ma0646@technikum-wien.at>
2025-04-16 10:53:26 +0200

    show a little popup div on anwCountDisplay hover that checkmark means anwesend; cross means abwesend and blue shielded person means entschuldigt

--- public/js/components/Lektor/AnwCountDisplay.js
+++ public/js/components/Lektor/AnwCountDisplay.js
@@ -1,6 +1,13 @@
 
 export const AnwCountDisplay = {
 	name: 'AnwCountDisplay',
+	data: function() {
+		return {
+			anwHovered: false,
+			abwHovered: false,
+			entHovered: false
+		}
+	},
 	props: {
 		anwesend: '/',
 		abwesend: '/',
@@ -10,11 +17,25 @@ export const AnwCountDisplay = {
 	
 		<div class="text-center">
 			<div style="display: flex; justify-content: center; align-items: center;">
-				<div style="color: #28a745;"><h3>{{anwesend}} <i class="fa fa-check"></i></h3></div>
+				<div @mouseover="anwHovered = true" @mouseleave="anwHovered = false" style="position: relative; display: inline-block; color: #28a745;">
+					<h3>{{anwesend}} <i class="fa fa-check"></i>
+						<div class="legend-info" v-show="anwHovered"> {{$capitalize($p.t('global/anwesend'))}}</div>
+					</h3>
+				</div>
 
-				<div style="color: #dc3545; margin-left: 36px;"><h3>{{abwesend}} <i class="fa fa-xmark"></i></h3></div>
+				<div @mouseover="abwHovered = true" @mouseleave="abwHovered = false" style="position: relative; display: inline-block; color: #dc3545; margin-left: 36px;">
+					<h3>{{abwesend}} <i class="fa fa-xmark"></i>
+						<div class="legend-info" v-show="abwHovered"> {{$capitalize($p.t('global/abwesend'))}}</div>
+					</h3>
+				</div>
 
-				<div v-if="this.$entryParams.permissions.entschuldigungen_enabled" style="color: #0335f5; margin-left: 36px;"><h3>{{entschuldigt}} <i class="fa-solid fa-user-shield"></i></h3></div>
+				<div @mouseover="entHovered = true" @mouseleave="entHovered = false" 
+				v-if="this.$entryParams.permissions.entschuldigungen_enabled" 
+				style="position: relative; display: inline-block; color: #0335f5; margin-left: 36px;">
+					<h3>{{entschuldigt}} <i class="fa-solid fa-user-shield"></i>
+						<div class="legend-info" v-show="entHovered"> {{$capitalize($p.t('global/entschuldigt'))}}</div>
+					</h3>
+				</div>
 
 			</div>
 		</div>
