193ad67a0655c13c2c5e4a17f98f7dfeccf678e7
Johann Hoffmann <ma0646@technikum-wien.at>
2026-02-12 14:42:19 +0100

    same LE has lessons twice per day - find correct anw checkIn entry by comparing datetime, not just date =)

--- models/Anwesenheit_User_model.php
+++ models/Anwesenheit_User_model.php
@@ -18,7 +18,7 @@ class Anwesenheit_User_model extends \DB_Model
 		$query = "
 			SELECT *
 			FROM extension.tbl_anwesenheit_user JOIN extension.tbl_anwesenheit USING (anwesenheit_id)
-			WHERE prestudent_id = ? AND lehreinheit_id = ? AND DATE(extension.tbl_anwesenheit.von) = ?
+			WHERE prestudent_id = ? AND lehreinheit_id = ? AND extension.tbl_anwesenheit.von = ?
 			ORDER BY von ASC;
 		";
 
